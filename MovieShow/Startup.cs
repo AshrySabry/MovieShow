cd27b98ed08752266d3a3c6602fa82430ece8bf5
AshrySabry <ashrysabry@gmail.com>
2021-06-30 21:52:38 +0800

    first commit

---
+++ MovieShow/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieShow.Startup))]
+namespace MovieShow
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
